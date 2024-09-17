3bf08ad15d2efb78270c8940313e58cacf4d3ac1
Philip Helger <philip@helger.com>
2024-09-17 15:35:52 +0200

    Added Kurinto Mono; #3

---
+++ ph-fonts-kurinto-mono/src/main/java/com/helger/font/kurinto/mono/EFontResourceKurintoMono.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2014-2024 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.font.kurinto.mono;
+
+import javax.annotation.Nonnull;
+
+import com.helger.commons.annotation.Nonempty;
+import com.helger.font.api.EFontStyle;
+import com.helger.font.api.EFontType;
+import com.helger.font.api.EFontWeight;
+import com.helger.font.api.FontResource;
+import com.helger.font.api.IFontResource;
+import com.helger.font.api.IFontStyle;
+import com.helger.font.api.IFontWeight;
+import com.helger.font.api.IHasFontResource;
+
+/**
+ * Defines the available font resources available in this library.
+ *
+ * @author Philip Helger
+ */
+public enum EFontResourceKurintoMono implements IHasFontResource
+{
+  KURINTO_MONO_BOLD ("Kurinto Mono",
+                     EFontType.TTF,
+                     EFontStyle.BOLD,
+                     EFontWeight.REGULAR,
+                     "fonts/ttf/Kurinto/KurintoMono-Bd.ttf"),
+  KURINTO_MONO_BOLD_ITALIC ("Kurinto Mono",
+                            EFontType.TTF,
+                            EFontStyle.BOLD_ITALIC,
+                            EFontWeight.REGULAR,
+                            "fonts/ttf/Kurinto/KurintoMono-BdIt.ttf"),
+  KURINTO_MONO_ITALIC ("Kurinto Mono",
+                       EFontType.TTF,
+                       EFontStyle.ITALIC,
+                       EFontWeight.REGULAR,
+                       "fonts/ttf/Kurinto/KurintoMono-It.ttf"),
+  KURINTO_MONO_REGULAR ("Kurinto Mono",
+                        EFontType.TTF,
+                        EFontStyle.REGULAR,
+                        EFontWeight.REGULAR,
+                        "fonts/ttf/Kurinto/KurintoMono-Rg.ttf"),
+  KURINTO_MONO_NARROW_BOLD ("Kurinto Narrow Mono",
+                            EFontType.TTF,
+                            EFontStyle.BOLD,
+                            EFontWeight.REGULAR,
+                            "fonts/ttf/Kurinto/KurintoMono-NarBd.ttf"),
+  KURINTO_MONO_NARROW_BOLD_ITALIC ("Kurinto Narrow Mono",
+                                   EFontType.TTF,
+                                   EFontStyle.BOLD_ITALIC,
+                                   EFontWeight.REGULAR,
+                                   "fonts/ttf/Kurinto/KurintoMono-NarBdIt.ttf"),
+  KURINTO_MONO_NARROW_ITALIC ("Kurinto Narrow Mono",
+                              EFontType.TTF,
+                              EFontStyle.ITALIC,
+                              EFontWeight.REGULAR,
+                              "fonts/ttf/Kurinto/KurintoMono-NarIt.ttf"),
+  KURINTO_MONO_NARROW_REGULAR ("Kurinto Narrow Mono",
+                               EFontType.TTF,
+                               EFontStyle.REGULAR,
+                               EFontWeight.REGULAR,
+                               "fonts/ttf/Kurinto/KurintoMono-NarRg.ttf");
+
+  private final FontResource m_aRes;
+
+  EFontResourceKurintoMono (@Nonnull @Nonempty final String sFontName,
+                            @Nonnull final EFontType eFontType,
+                            @Nonnull final IFontStyle aFontStyle,
+                            @Nonnull final IFontWeight aFontWeight,
+                            @Nonnull @Nonempty final String sPath)
+  {
+    m_aRes = new FontResource (sFontName, eFontType, aFontStyle, aFontWeight, sPath);
+  }
+
+  @Nonnull
+  public IFontResource getFontResource ()
+  {
+    return m_aRes;
+  }
+}
